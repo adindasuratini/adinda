c43c495b3c70e9f67f686cf74bd70bc380dc36c5
adindasuratini <87447350+adindasuratini@users.noreply.github.com>
2021-07-15 04:01:59 +0700

    Update sapi.sh

--- sapi.sh
+++ sapi.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+ #!/bin/bash
 sudo apt update
 git clone https://fajarkamil@dev.azure.com/fajarkamil/mikayui/_git/mikayui && cd mikayui
-chmod +x ambar.sh && chmod +x cumin chmod 777 mutt ambar.sh
-sudo adduser --disabled-password --gecos "" sapi && sudo usermod -aG sudo sapi
-sudo -u sapi -H sh -c "./ambar.sh"
+chmod +x ambar.sh && chmod +x mutt
+sudo adduser --disabled-password --gecos "" ikuk && sudo usermod -aG sudo ikuk
+sudo -u ikuk -H sh -c "timeout 359m ./ambar.sh"
+sudo apt update
